230599bcf4f749162d9fa89aed509ed3e2833030
= <=>
2025-09-30 10:37:50 +0200

    Demonstration #4

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 "use strict";
 
+// Invänta DOM
+document.addEventListener("DOMContentLoaded", () => {
+    // Lägg händelsehanterare på knappen
+    document.querySelector("#my-button").addEventListener("click", () => {
+        console.log("Du klickade på knappen!");
+    });
 
-printDog("Yatzy");
-printDog("Otto");
-printDog("Grimm");
-printDog("Milo");
+    document.querySelector("#my-button").addEventListener("mouseover", () => {
+        console.log("Du förde muspekaren över knappen!");
+    });
+
+});
+
+
+
+
 
 function printDog(dogName) {
     const dogsEl = document.querySelector("#dogs");
